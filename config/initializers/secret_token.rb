0e88c802b07f44e9556567ace2bd6e494852e5bc
Hubert Liu <hubert.liu@rigor.com>
2013-09-19 15:00:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenvpnConf::Application.config.secret_key_base = '6ac704c1c6ec110a979afc707f3137ba9bc402632dd46d977056e971ee4961b4fa673a7201d05a106087ad606b0629b2e544c47e4c8db048f392a213326a56a3'
